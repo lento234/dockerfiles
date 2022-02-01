1ff32382a9e6acc1e10500f7dcc69345484c7cfa
Mrlento234 <lento.manickathan@gmail.com>
2022-02-01 15:24:08 +0100

    improved build config, now possible to build single image

--- Makefile
+++ Makefile
@@ -6,9 +6,12 @@ PHONY: help
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
-.PHONY: build
-build: ## Build and push all docker images
-	@$(CURDIR)/build.sh
+.PHONY: all
+all: ## Build and push all docker images
+	@$(CURDIR)/build.sh all
+
+build: ## Build and push a docker images (arg: IMAGE=<image>)
+	@$(CURDIR)/build.sh $(IMAGE)
 
 .PHONY: interact
 interact: ## Interact with a docker image (arg: IMAGE=<image>)
--- build.sh
+++ build.sh
@@ -6,34 +6,53 @@ USER=mrlento234
 
 build_image () {
     IMAGE=$1
+   
+    cd $IMAGE
 
+    echo -e "\e[31m Building \e[1m${USER}/${IMAGE}\e[0m\e[31m ...\e[0m"
 	DOCKER_BUILDKIT=1 docker build --progress=auto -t $USER/$IMAGE .
+    echo -e "\e[32m Build \e[1m${USER}/${IMAGE}\e[0m\e[32m.\e[0m\n"
+    
+    cd ..
 
 }
 
 push_image () {
     IMAGE=$1
+    
+    cd $IMAGE
 
+    echo -e "\e[31m Pushing \e[1m${USER}/${IMAGE}\e[0m\e[31m ...\e[0m"
     docker push $USER/$IMAGE:latest
+    echo -e "\e[32m Pushed \e[1m${USER}/${IMAGE}\e[0m\e[32m.\e[0m\n"
+    
+    cd ..
+
 }
 
-build_push_all () {
+build_and_push_all () {
     
     # get the dockerfiles
     for i in `find . -name "Dockerfile" | cut -d '/' -f2`
     do
-        # Building images
-        echo -e "\e[31m Building \e[1m${USER}/${i}\e[0m\e[31m ...\e[0m"
-        cd $i
         build_image $i
-        cd ..
-        echo -e "\e[32m Build \e[1m${USER}/${i}\e[0m\e[32m.\e[0m\n"
-
-        # Pushing images to dockerhub
         push_image $i
-        echo -e "\e[32m Pushed \e[1m${USER}/${i}\e[0m\e[32m.\e[0m\n"
     done
 
 }
 
-build_push_all
+build_and_push () {
+    build_image $1
+    push_image $1
+}
+
+main () {
+    if [ $1 == "all" ]; then
+        build_and_push_all $1
+    else
+        build_and_push $1
+    fi
+}
+
+
+main $1
